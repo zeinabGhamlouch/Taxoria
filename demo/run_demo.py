1db7c84ed3a5b82df9b1c7410a99a035b99a71d6
zeinabGhamlouch <zeinab.b.ghamloush@gmail.com>
2025-05-02 11:51:17 +0200

    Update run_demo.py

--- demo/run_demo.py
+++ demo/run_demo.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from src.enrich_taxonomy import traverse_and_add_children, load_json_from_file, max_depth
 import json
 
@@ -10,6 +14,8 @@ taxonomy = load_json_from_file(input_path)
 
 # Enrich it
 depth = max_depth(taxonomy) - 1
+print(f"Max depth of the tree: {depth}")
+
 taxonomy = traverse_and_add_children(taxonomy, max_depth=depth)
 
 # Save output
